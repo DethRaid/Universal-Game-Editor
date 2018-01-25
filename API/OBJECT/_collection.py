67b561b7f034747b1a99df8a2018e8d646de3998
Tcll <tcll5850@gmail.com>
2018-01-25 13:23:24 -0500

    / ‘API/OBJECT/_collection.py’ / ‘projectfiles/UGE/.idea/workspace.xml’

--- API/OBJECT/_collection.py
+++ API/OBJECT/_collection.py
@@ -400,8 +400,9 @@ del private
 from ..OBJECT import properties
 def CollectionProp( cls: object, attr: str ) -> function:
     """reassigns a collection verification property to an existing member_descriptor attribute"""
+    name = cls.__name__; initializers = properties[name] = properties.get(name,set())
     dsc = cls.__dict__[attr]
-    dscget = dsc.__get__
+    dscget = dsc.__get__; dscset = dsc.__set__
     def setter(obj, val) -> None: """verify and set a collection"""; dscget(obj,cls)[:] = val
-    property( dscget, setter )
+    setattr(cls,attr,property( dscget, setter ))
     return dsc.__set__
