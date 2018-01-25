e94a45c0418ec938e2e88ebf5ef8005fda459cf9
Tcll <tcll5850@gmail.com>
2018-01-25 07:39:46 -0500

    / ‘API/OBJECT/_channels.py’ / ‘projectfiles/UGE/.idea/workspace.xml’

--- API/OBJECT/_channels.py
+++ API/OBJECT/_channels.py
@@ -1,9 +1,33 @@
 # -*- coding: utf-8 -*-
 
-from . import UGEObjectConstructor, new
+def private():
+    new = object.new
+    class UGEChannels(object):
+        def __new__( cls ):
+            ch = new(cls)
+            def call(*items: [str,int,None]) -> UGEObject:
+                """create a new or reference an existing collection."""
+                if len(items) > 1:
+                    channel, item, *other = items
+                    item    = getattr(item,'__value__',item)
+                else: channel, = items; item = ...
+                channel = getattr(channel,'__value__',channel)
+                
+                channels = getchannels(ch)
+                cl = channels.get(channel,None)
+                if cl is None: cl = channels[channel] = UGECollection(Parent,Base,**kw)
+                
+                if item is not ...: return cl(item)
+                return cl
+            
+            ch.__call__ = method(call,ch)
+            return ch
+        
+    return UGEChannels
 
-class UGEChannels(object):
-    pass
+
+UGEChannels = private()
+del private
 
 def ChannelsProp( cls: object, attr: str ) -> function:
     """reassigns a collection verification property to an existing member_descriptor attribute"""
