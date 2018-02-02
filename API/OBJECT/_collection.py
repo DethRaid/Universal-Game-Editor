c2afbb1705b80cae8ba879f9fb7a62bf7a13a2de
Tcll <tcll5850@gmail.com>
2018-02-02 08:31:17 -0500

    / ‘API/OBJECT/_collection.py’

--- API/OBJECT/_collection.py
+++ API/OBJECT/_collection.py
@@ -216,15 +216,15 @@ del private
 from ..OBJECT import properties
 def CollectionProp( cls: object, attr: str, base: object, **kw ):
     """reassigns a collection verification property to an existing member_descriptor attribute"""
-    name = cls.__name__; instatiators = properties[name] = properties.get(name,set())
+    name = cls.__name__; initializers = properties[name] = properties.get(name,set())
     dsc = cls.__dict__[attr]
     dscget = dsc.__get__; dscset = dsc.__set__
     def setter(obj, val) -> None: """verify and set a collection"""; dscget(obj,cls)[:] = val
     setattr(cls,attr,property( dscget, setter ))
     strbase = base.__class__ is str
-    def instatiator(obj):
-        """called when the owning class is instantiated"""
+    def init(obj):
+        """called when the owning class is initialized"""
         # NOTE: obj.__owner__ is for UGEObject sub-type objects to link back to the owner when referenced from (otherwize the owner is itself).
         own = getattr(obj,'__owner__',obj)
         dscset(obj, UGECollection( own, getattr(own.__parents__[base],attr) if strbase else base, **kw ))
-    instatiators.add(instatiator)
+    initializers.add(init)
