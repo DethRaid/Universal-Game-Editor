755e6ec43129a693d69afe07a64b9fd2366822b5
Tcll <tcll5850@gmail.com>
2018-02-07 10:34:19 -0500

    / ‘API/OBJECT/__init__.py’

--- API/OBJECT/__init__.py
+++ API/OBJECT/__init__.py
@@ -30,7 +30,9 @@ def private():
             
             ugeobj = newType(meta, name, bases, NS) # TODO: mappingproxy(NS))
             if 'Name' in slots: properties[name]['Name'] = ugeobj.Name.__set__
-            if 'Index' in slots: properties[name]['Index'] = ugeobj.Index.__set__
+            if 'Index' in slots:
+                properties[name]['Index'] = ugeobj.Index.__set__
+                ugeobj.Index = property(ugeobj.Index.__get__)
             defined.add(name); return ugeobj
 
     class UGEObject(object, metaclass=UGEObjectConstructor):
