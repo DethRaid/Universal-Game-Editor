ebb3ead856a53e93fba8ab8e57b247d6ac6fe229
Tcll <tcll5850@gmail.com>
2018-02-07 22:09:58 -0500

    / ‘API/FORMAT/_material.py’

--- API/FORMAT/_material.py
+++ API/FORMAT/_material.py
@@ -23,7 +23,7 @@ def private():
             return Ma
 
         def new(cls, parents: mappingproxy, holder: UGECollection, item, external=False, *args, **kw):
-            """Create a new primitive instance of the specified type"""
+            """Create a new material instance"""
             Ma = cls.__new__(parents,holder,*args,**kw)
             item = getattr(item,'__value__',item) # from UGE data-type (struct or such)
             if item.__class__ is dict: Ma[:] = item
